544b6a0628a2d8e69b61dffb4e38c81be785eef6
Sven Paulsen <info@sven-paulsen.de>
2023-10-10 16:57:22 +0200

    use specified timeout for server link expiry

--- redis/server_lib.lua
+++ redis/server_lib.lua
@@ -4,10 +4,12 @@
 ---@param key          string  Key to the `room::{[room-id]}::server` entry.
 ---@param server-id    string  The id of the server.
 ---@param instance-id  string  The id of the instance.
+---@param timeout      number  The timeout for the link in seconds.
 local function server_link_room(keys, args)
     local key = keys[1]
     local server_id = args[1]
     local instance_id = args[2]
+    local timeout = args[3]
     local value = redis.call('HGET', key, 'id')
     if value == false then
         redis.call('HSET', key, 'id', server_id, 'instance-id', instance_id)
@@ -16,7 +18,7 @@ local function server_link_room(keys, args)
     else
         return 0
     end
-    redis.call('EXPIRE', key, 86400) -- 1 day
+    redis.call('EXPIRE', key, timeout)
     redis.call('TYPE', key)
     return 1
 end
