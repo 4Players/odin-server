06f73974d01a74d3c57899bbbaca24449a06db90
Sven Paulsen <info@sven-paulsen.de>
2024-06-04 14:05:11 +0200

    update docker entrypoint script

--- docker/README.md
+++ docker/README.md
@@ -9,17 +9,75 @@ By default, the images expect a single volume, located at `/data`, that will hol
 * a server configuration file (`config.toml`)
 * a server license file (`license.toml`)
 * a PEM-encoded certificate file (`fullchain.pem`)
-* a PEM-encoded private key file (`fullchain.pem`)
+* a PEM-encoded private key file (`privkey.pem`)
 
 ### Auto-Generating a Configuration File
 
-If no configuration file is found in `/data`, the image will try to auto-generate a minimal `config.toml` using environment variables.
+If no configuration file is found in `/data`, the image will try to auto-generate a `config.toml` using environment variables.
 
 The following environment variables are supported:
 
-* `GRID_PUBLIC_ADDRESS` (mandatory): The FQDN and port number where the server can be reached from the Internet
-* `GRID_SUPERVISOR_URL` (mandatory): The full web address of the supervisor to contact and send reports to
-* `GRID_CERTIFICATE_FILE`: The path to a PEM-encoded leaf certificate followed by optional intermediate certificates.
-* `GRID_PRIVATE_KEY_FILE`: The path to a PEM-encoded leaf certificate followed by optional intermediate certificates.
-* `GRID_VERBOSITY`: The verbosity level `0-3` to define how much information should be written to logs.
-* `GRID_DEPLOYMENT`: Deployment labels that are added to all reported metrics.
+* `GRID_PUBLIC_ADDRESS`
+* `GRID_CERTIFICATE_FILE`
+* `GRID_PRIVATE_KEY_FILE`
+* `GRID_DEPLOYMENT`
+
+#### Supervisor Settings
+
+* `GRID_SUPERVISOR_URL`
+* `GRID_SUPERVISOR_REPORT_INTERVAL`
+* `GRID_SUPERVISOR_NO_WARMUP`
+
+#### QUIC Settings
+
+* `GRID_QUIC_NO_PEERS_TIMEOUT`
+* `GRID_QUIC_KEEP_ALIVE_INTERVAL`
+* `GRID_QUIC_CONNECT_TIMEOUT`
+* `GRID_QUIC_PRE_SEND_TIMEOUT`
+* `GRID_QUIC_IDLE_TIMEOUT`
+* `GRID_QUIC_MIGRATION`
+
+#### HTTP Settings
+
+* `GRID_HTTP_WEBRTC_CANDIDATES`
+* `GRID_HTTP_NO_PEERS_TIMEOUT`
+* `GRID_HTTP_PING_INTERVAL`
+* `GRID_HTTP_TIMEOUT`
+
+#### Logging Settings
+
+* `GRID_LOG_VERBOSITY`
+* `GRID_LOG_FORMAT`
+* `GRID_LOG_FILTERS`
+* `GRID_LOG_LOKI_URL`
+* `GRID_LOG_LOKI_LABELS`
+
+#### Database Settings
+
+* `GRID_DATABASE_HOST`
+* `GRID_DATABASE_USERNAME`
+* `GRID_DATABASE_PASSWORD`
+* `GRID_DATABASE_SERVER_ID`
+* `GRID_DATABASE_COMMAND_TIMEOUT`
+* `GRID_DATABASE_USE_TLS`
+
+#### Limit Settings
+
+* `GRID_LIMIT_PEER_MEDIAS`
+* `GRID_LIMIT_PEER_INCOMING_MESSAGES`
+* `GRID_LIMIT_CONCURRENT_STREAMS`
+
+#### Metrics Settings
+
+* `GRID_METRICS_SYSTEM_UPDATE_INTERVAL`
+* `GRID_METRICS_IDLE_TIMEOUT`
+
+#### HTTP Proxy Settings
+
+* `GRID_HTTP_CLIENT_ADDRESS`
+* `GRID_HTTP_CLIENT_ACCEPT_INVALID_CERTIFICATES`
+
+#### Internal Settings
+
+* `GRID_INTERNAL_LOG_REJECTED_API_CALLS`
+* `GRID_INTERNAL_LOG_HANGING_API_CALLS`
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,29 +10,82 @@ if [ -f "/data/config.toml" ]; then
   cp -f "/data/config.toml" "${GRID_HOME}/config.toml"
 else
   cat << EOF | sed "s/^[ \t]*//;s/[ \t]*$//;/^$/d" > "${GRID_HOME}/config.toml"
-    public_address = "${GRID_PUBLIC_ADDRESS}"
+      ${GRID_PUBLIC_ADDRESS:+public_address = \"${GRID_PUBLIC_ADDRESS}\"}
 
-    [log]
-    verbosity = ${GRID_VERBOSITY:-0}
+      [log.terminal]
+      verbosity = ${GRID_LOG_VERBOSITY:-0}
+      format = "${GRID_LOG_FORMAT:-text}"
+      ${GRID_LOG_FILTERS:+filters = [ ${GRID_LOG_FILTERS} ]}
 
-    [supervisor]
-    url = "${GRID_SUPERVISOR_URL}"
+      ${GRID_LOG_LOKI_URL:+
+        [log.loki]
+        url = \"${GRID_LOG_LOKI_URL}\"
+        ${GRID_LOG_LOKI_LABELS:+labels = ${GRID_LOG_LOKI_LABELS}}
+      }
 
-    [quic]
-    binding = "0.0.0.0:4433"
-    certificate_file = "${GRID_CERTIFICATE_FILE:-/data/fullchain.pem}"
-    privatekey_file = "${GRID_PRIVATE_KEY_FILE:-/data/privkey.pem}"
+      ${GRID_PROVISIONING_URL:+
+        [provisioning]
+        url = \"${GRID_PROVISIONING_URL}\"
+      }
 
-    [http]
-    binding = "0.0.0.0:4433"
-    certificate_file = "${GRID_CERTIFICATE_FILE:-/data/fullchain.pem}"
-    privatekey_file = "${GRID_PRIVATE_KEY_FILE:-/data/privkey.pem}"
-    webrtc_binding = "0.0.0.0:5000"
+      ${GRID_DATABASE_URL:+
+        [database]
+        url = \"${GRID_DATABASE_URL}\"
+        ${GRID_DATABASE_USERNAME:+username = \"${GRID_DATABASE_USERNAME}\"}
+        ${GRID_DATABASE_PASSWORD:+password = \"${GRID_DATABASE_PASSWORD}\"}
+        ${GRID_DATABASE_SERVER_ID:+server_id = \"${GRID_DATABASE_SERVER_ID}\"}
+        ${GRID_DATABASE_COMMAND_TIMEOUT:+command_timeout = ${GRID_DATABASE_COMMAND_TIMEOUT}}
+        ${GRID_DATABASE_REPORT_INTERVAL:+report_interval = ${GRID_DATABASE_REPORT_INTERVAL}}
+      }
 
-    [metrics]
-    binding = "0.0.0.0:9000"
-    global_labels = { deployment = "${GRID_DEPLOYMENT:-docker}", server = "${GRID_PUBLIC_ADDRESS}" }
-    allowed = ["0.0.0.0/0"]
+      [authorization]
+      ${GRID_AUTHORIZATION_LEEWAY:+leeway = ${GRID_AUTHORIZATION_LEEWAY}}
+      allow_unsigned = ${GRID_AUTHORIZATION_ALLOW_UNSIGNED:-false}
+      keys = [ ${GRID_AUTHORIZATION_KEYS} ]
+
+      [quic]
+      binding = "0.0.0.0:4433"
+      certificate_file = "${GRID_CERTIFICATE_FILE:-/data/fullchain.pem}"
+      privatekey_file = "${GRID_PRIVATE_KEY_FILE:-/data/privkey.pem}"
+      ${GRID_QUIC_NO_PEERS_TIMEOUT:+no_peers_timeout = ${GRID_QUIC_NO_PEERS_TIMEOUT}}
+      ${GRID_QUIC_KEEP_ALIVE_INTERVAL:+keep_alive_interval = ${GRID_QUIC_KEEP_ALIVE_INTERVAL}}
+      ${GRID_QUIC_CONNECT_TIMEOUT:+connect_timeout = ${GRID_QUIC_CONNECT_TIMEOUT}}
+      ${GRID_QUIC_PRE_SEND_TIMEOUT:+pre_send_timeout = ${GRID_QUIC_PRE_SEND_TIMEOUT}}
+      ${GRID_QUIC_IDLE_TIMEOUT:+idle_timeout = ${GRID_QUIC_IDLE_TIMEOUT}}
+      ${GRID_QUIC_MIGRATION:+migration = ${GRID_QUIC_MIGRATION}}
+
+      [http]
+      binding = "0.0.0.0:4433"
+      certificate_file = "${GRID_CERTIFICATE_FILE:-/data/fullchain.pem}"
+      privatekey_file = "${GRID_PRIVATE_KEY_FILE:-/data/privkey.pem}"
+      webrtc_binding = "0.0.0.0:5000"
+      ${GRID_HTTP_WEBRTC_CANDIDATES:+webrtc_candidates = [ ${GRID_HTTP_WEBRTC_CANDIDATES} ]}
+      ${GRID_HTTP_NO_PEERS_TIMEOUT:+no_peers_timeout = ${GRID_HTTP_NO_PEERS_TIMEOUT}}
+      ${GRID_HTTP_PING_INTERVAL:+ping_interval = ${GRID_HTTP_PING_INTERVAL}}
+      ${GRID_HTTP_TIMEOUT:+timeout = ${GRID_HTTP_TIMEOUT}}
+
+      ${GRID_HTTP_CLIENT_ADDRESS:+
+        [http_client]
+        address = \"${GRID_HTTP_CLIENT_ADDRESS}\"
+        ${GRID_HTTP_CLIENT_ACCEPT_INVALID_CERTIFICATES:+accept_invalid_certificates = ${GRID_HTTP_CLIENT_ACCEPT_INVALID_CERTIFICATES}}
+      }
+
+      [limit.peer]
+      medias = ${GRID_LIMIT_PEER_MEDIAS:-5}
+      ${GRID_LIMIT_CONCURRENT_STREAMS:+concurrent_streams = ${GRID_LIMIT_CONCURRENT_STREAMS}}
+      ${GRID_LIMIT_PEER_INCOMING_MESSAGES:+incoming_messages = ${GRID_LIMIT_PEER_INCOMING_MESSAGES}}
+
+      [metrics]
+      binding = "0.0.0.0:9000"
+      allowed = ["0.0.0.0/0"]
+      global_labels = { deployment = "${GRID_DEPLOYMENT:-docker}", server = "${GRID_PUBLIC_ADDRESS}" }
+      ${GRID_METRICS_SYSTEM_UPDATE_INTERVAL:+system_update_interval = ${GRID_METRICS_SYSTEM_UPDATE_INTERVAL}}
+      ${GRID_METRICS_IDLE_TIMEOUT:+idle_timeout = ${GRID_METRICS_IDLE_TIMEOUT}}
+
+      [internal]
+      log_rejected_api_calls = ${GRID_INTERNAL_LOG_REJECTED_API_CALLS:-false}
+      ${GRID_INTERNAL_LOG_HANGING_API_CALLS:+log_hanging_api_calls = ${GRID_INTERNAL_LOG_HANGING_API_CALLS}}
+
 EOF
 fi
 
